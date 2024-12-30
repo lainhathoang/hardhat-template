7b13f16b40fd747b77462009b13b1dfe74aee6e4
lainhathoang <nhathoang8c01@gmail.com>
2024-12-30 15:12:56 +0700

    update: add solhint

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -5,13 +5,18 @@ pragma solidity >=0.8.17;
 // import "hardhat/console.sol";
 
 contract Lock {
-    uint public unlockTime;
+    uint256 public unlockTime;
     address payable public owner;
 
-    event Withdrawal(uint amount, uint when);
+    event Withdrawal(uint256 amount, uint256 when);
 
-    constructor(uint _unlockTime) payable {
-        require(block.timestamp < _unlockTime, "Unlock time should be in the future");
+    error UnlockTimeInFuture();
+    error NotOwner();
+
+    constructor(uint256 _unlockTime) payable {
+        if (block.timestamp >= _unlockTime) {
+            revert UnlockTimeInFuture();
+        }
 
         unlockTime = _unlockTime;
         owner = payable(msg.sender);
@@ -21,8 +26,12 @@ contract Lock {
         // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
         // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+        if (block.timestamp < unlockTime) {
+            revert UnlockTimeInFuture();
+        }
+        if (msg.sender != owner) {
+            revert NotOwner();
+        }
 
         emit Withdrawal(address(this).balance, block.timestamp);
 
